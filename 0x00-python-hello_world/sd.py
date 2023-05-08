5406f182fb6ea141191774f0796233daf34acc4a
bakiizese <bereketzeselassie@gmail.com>
2023-05-08 14:35:31 -0700

    this try

---
+++ 0x00-python-hello_world/sd.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+i = "is better than"
+f = "If the implementation is"
+t = "to explain,"
+d = "idea"
+print(f"The Zen of Python, by Tim Peters\
+\n\nBeautiful {i} ugly.\
+\nExplicit {i} implicit.\
+\nSimple {i} complex.\
+\nComplex {i} complicated.\
+\nFlat {i} nested.\
+\nSparse {i} dense.\
+\nReadability counts.\
+\nSpecial cases aren't special enough to break the rules.\
+\nAlthough practicality beats purity.\
+\nErrors should never pass silently.\
+\nUnless explicitly silenced.\
+\nIn the face of ambiguity, refuse the temptation to guess.\
+\nThere should be one-- and preferably only one --obvious way to do it.\
+\nAlthough that way may not be obvious at first unless you're Dutch.\
+\nNow {i} never.\
+\nAlthough never is often better than *right* now.\
+\n{f} hard {t} it's a bad {d}.\
+\n{f} easy {t} it may be a good {d}.\
+\nNamespaces are one honking great {d} -- let's do more of those!")
