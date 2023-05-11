9e58d898a93e872500e562ccd8dfa1a9b0b26ba3
bakiizese <bereketzeselassie@gmail.com>
2023-05-11 14:03:34 -0700

    fixed

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -2,7 +2,7 @@
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
 
-    if a > a:
+    if b > a:
         j = add(a, b)
         for i in range(4, 6):
             j = add(j, i)
