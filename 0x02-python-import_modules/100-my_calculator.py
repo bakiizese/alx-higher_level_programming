7c7bdc98c43128a7e9188b86afe922f55149ec6a
bakiizese <bereketzeselassie@gmail.com>
2023-05-11 07:06:49 -0700

    advance

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, div, mul
+    import sys
+
+    ln = len(sys.argv)
+    if ln != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    a = int(sys.argv[1])
+    p = sys.argv[2]
+    b = int(sys.argv[3])
+    if p == '*':
+        print("{} {} {} = {}".format(a, p, b, mul(a, b)))
+    elif p == '/':
+        print("{} {} {} = {}".format(a, p, b, div(a, b)))
+    elif p == '-':
+        print("{} {} {} = {}".format(a, p, b, sub(a, b)))
+    elif p == '+':
+        print("{} {} {} = {}".format(a, p, b, add(a, b)))
+    else:
+        print("Unknown operator. Availbale operators: +, -, * and /")
+        exit(1)
---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    from magic_calculation_102 import add, sub
+
+    if a > a:
+        j = add(a, b)
+        for i in range(4, 6):
+            j = add(j, i)
+        return j
+    else:
+        return sub(a, b)
