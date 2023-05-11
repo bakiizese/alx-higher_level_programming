dc51ab685dbf64fb1c54f8ba6c70fbaf9d5acbf1
bakiizese <bereketzeselassie@gmail.com>
2023-05-11 05:36:39 -0700

    import

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    
+    i = len(sys.argv)
+    if i == 1:
+        print("{} argument:".format(i-1))
+    elif i == 2:
+        print("{} arguments:".format(i-1))
+    else:
+        print("{} arguments:".format(i-1))
+    for j in range(1, i):
+        print(f"{j}: {sys.argv[j]}")
