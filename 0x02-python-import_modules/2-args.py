4c53d787bb4ce4fa7914bc24695c72cb91cbdf89
bakiizese <bereketzeselassie@gmail.com>
2023-05-11 05:38:20 -0700

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    
+
     i = len(sys.argv)
     if i == 1:
-        print("{} argument:".format(i-1))
+        print("{} arguments.".format(i-1))
     elif i == 2:
         print("{} arguments:".format(i-1))
     else:
