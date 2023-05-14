07287c39ff27d7497fdaea38680deaefccb85674
bakiizese <bereketzeselassie@gmail.com>
2023-05-14 13:21:00 -0700

    pasing

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <Python.h>
+/**
+ * print_python_list_info - entry
+ * @p: struct var
+ */
+void print_python_list_info(PyObject *p)
+{
+	int n;
+
+	printf("[*] Size of Python List = %lu\n", Py_SIZE(p));
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (n = 0; n < Py_SIZE(p); n++)
+		printf("Element %d: %s\n", n, Py_TYPE(PyList_GetItem(p, n))->tp_name);
+}
