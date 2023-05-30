d84ffb508438d31d9864f9d7c95648a518d3a5c1
bakiizese <bereketzeselassie@gmail.com>
2023-05-30 08:43:01 -0700

    fix

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -60,18 +60,18 @@ class SinglyLinkedList:
             new.next_node = self.__head
             self.__head = new
         else:
-            tmp = self.__head
-            while (tmp.next_node is not None and
-                    tmp.next_node.data < value):
-                tmp = tmp.next_node
-            new.next_node = tmp.next_node
-            tmp.next_node = new
+            p = self.__head
+            while (p.next_node is not None and
+                    p.next_node.data < value):
+                p = p.next_node
+            new.next_node = p.next_node
+            p.next_node = new
 
     def __str__(self):
         """Define the print() representation of a SinglyLinkedList."""
-        values = []
-        tmp = self.__head
-        while tmp is not None:
-            values.append(str(tmp.data))
-            tmp = tmp.next_node
-        return ('\n'.join(values))
+        vr = []
+        p = self.__head
+        while p is not None:
+            vr.append(str(p.data))
+            p = p.next_node
+        return ('\n'.join(vr))
