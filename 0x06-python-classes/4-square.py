301bbc1a6e12cc5a77aca2ce05af2f6d3461744d
bakiizese <bereketzeselassie@gmail.com>
2023-05-30 08:21:59 -0700

    fix

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
+
+"""Define a class Square."""
+
+
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.size = size
 
     def size(self):
+        """Get/set the current size of the square."""
         return (self.__size)
 
     def size(self, value):
@@ -14,4 +26,5 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return the current area of the square."""
         return (self.__size * self.__size)
