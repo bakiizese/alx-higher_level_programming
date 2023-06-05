bd8685d5f42d9ed86d8626e7270e4648622d15be
bakiizese <bereketzeselassie@gmail.com>
2023-06-05 13:08:22 -0700

    1 rec

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+'''a real rectangle class'''
+
+
+class Rectangle:
+    '''presenting a rectangle class'''
+    def __init__(self, height=0, width=0):
+        '''an init method'''
+        self.height = height
+        self.width = width
+
+    @property
+    def width(self):
+        '''width method'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+            self.__width = value
+
+    @property
+    def height(self):
+        '''height method'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
