3867893121f7e2128fbba7d2cb70daccd6d9d2a5
bakiizese <bereketzeselassie@gmail.com>
2023-10-20 08:22:01 -0700

    ix

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -9,5 +9,5 @@ class Square(Rectangle):
     def __init__(self, size):
         """Initialize new"""
         self.integer_validator("size", size)
-        super().__init__(size, size)
+        self.__init__(size, size)
         self.__size = size
