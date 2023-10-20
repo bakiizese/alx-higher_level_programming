c3494ed46f30a6626882bb91de1991dcd5c0c668
bakiizese <bereketzeselassie@gmail.com>
2023-10-19 21:33:03 -0700

    try

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,58 +1,44 @@
 #!/usr/bin/python3
-'''a real rectangle class'''
-
 
 class Rectangle:
-    '''presenting a rectangle class'''
-
-    def __init__(self, width=0, height=0):
-        '''initial rectangle'''
-        self.width = width
-        self.height = height
+    def __init__(self, width = 0, height = 0):
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        '''width method'''
         return self.__width
-
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError("must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("is less than 0")
         self.__width = value
 
     @property
     def height(self):
-        '''height method'''
         return self.__height
-
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError("must be an integer")
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("is less than 0")
         self.__height = value
 
     def area(self):
-        '''return area'''
-        return (self.__width * self.__height)
+        return self.__width * self.__height
 
     def perimeter(self):
-        '''return perimeter'''
-        if self.__width == 0 or self.__height == 0:
-            return (0)
-        return ((self.__width * 2) + (self.__height * 2))
+        return 2 * (self.__width * self.__height)
 
     def __str__(self):
-        '''print #'''
-        if self.__width == 0 or self.height == 0:
-            return("")
-        r = []
+        if self.__width == 0 or self.__height == 0:
+            return ('')
         for i in range(self.__height):
-            [r.append("#") for k in range(self.__width)]
+            for j in range(self.__width):
+                print("#", end = '')
             if i != self.__height - 1:
-                r.append("\n")
-        return ("".join(r))
+                print('')
+        return ('')
