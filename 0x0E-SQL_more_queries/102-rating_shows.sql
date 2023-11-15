182c003a6d051d73d9a1c023f9a0001bec2b048a
bakiizese <bereketzeselassie@gmail.com>
2023-11-15 13:10:34 -0800

    rate

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,11 +1,8 @@
 -- rating
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS i
-	FROM tv_genres
-	JOIN tv_show_genres
-	ON tv_genres.id = tv_show_genres.genre_id
-	JOIN tv_shows
-	ON tv_shows.id = tv_show_genres.show_id
-	JOIN tv_show_ratings
-	ON tv_shows.id = tv_show_ratings.show_id
-	GROUP BY tv_genres.name
-	ORDER BY i DESC;
+SELECT tv_shows.title,
+       SUM(tv_show_ratings.rate) AS rating
+       FROM tv_shows
+       INNER JOIN tv_show_ratings
+       ON tv_show_ratings.show_id = tv_shows.id
+       GROUP BY tv_shows.title
+       ORDER BY rating DESC;
