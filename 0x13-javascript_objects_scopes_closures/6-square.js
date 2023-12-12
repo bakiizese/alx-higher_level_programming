61de120c473b0c56cd4eb08359e73f045a5af4b6
bakiizese <bereketzeselassie@gmail.com>
2023-12-12 12:00:27 -0800

    fix

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -10,7 +10,7 @@ class Square extends Rectangle {
     if (c === undefined) {
       this.print();
     } else {
-    for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
+      for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
 }
