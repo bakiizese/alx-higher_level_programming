36a6ea5f31d9899cc06beb72844b56a9921717fe
bakiizese <bereketzeselassie@gmail.com>
2023-12-15 02:33:33 -0800

    fix

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""cities forigne key"""
+
+
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    ms = sys.argv[4]
+    cur.execute("""SELECT cities.name FROM cities
+                   INNER JOIN states ON states.id=cities.state_id WHERE
+                   states.name LIKE BINARY %s ORDER BY state_id""", (ms, ))
+    row = cur.fetchall()
+    for i in row:
+        for j in i:
+            print(j + ', ', end='')
+    print('')
+    cur.close()
+    db.close()
