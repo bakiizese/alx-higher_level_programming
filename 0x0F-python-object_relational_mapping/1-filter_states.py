a82e5d79edd00ee3c1b33d6a746653f449654a96
bakiizese <bereketzeselassie@gmail.com>
2023-12-14 23:54:06 -0800

    fic

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+"""filter by N first letter"""
+
 
 if __name__ == "__main__":
     import sys
     import MySQLdb
 
-    db = MySQLdb.connect(host='localhost', port=3306 , user=sys.argv[1],
-                  passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'
+                ORDER BY states.id")
     rows = cur.fetchall()
     for row in rows:
         print(row)
