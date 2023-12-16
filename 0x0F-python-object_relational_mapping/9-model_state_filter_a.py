8766146d4bd4395dcb12aaa1e05fcd454e4e394a
bakiizese <bereketzeselassie@gmail.com>
2023-12-16 08:02:25 -0800

    fix

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''searcch that conatain 'a'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+    '''search that conatain 'a'''
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    stat = session.query(State).order_by(State.id)
+    stat = stat.filter(State.name.contains('a')).all()
+    for st in stat:
+        print('{}: {}'.format(st.id, st.name))
