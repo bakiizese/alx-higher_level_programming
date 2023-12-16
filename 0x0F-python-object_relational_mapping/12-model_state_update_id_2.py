8d3a8d813744738a016b4ed7be54b8af95389da7
bakiizese <bereketzeselassie@gmail.com>
2023-12-16 08:30:16 -0800

    fix

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''searcch that conatain 'a'''
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+
+if __name__ == '__main__':
+    '''search that conatain 'a'''
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    stat = session.query(State).filter(State.id == 2).first()
+    stat.name = "New Mexico"
+    session.commit()
