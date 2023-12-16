3c6101150dc982e66f627863da110ad7ff45113d
bakiizese <bereketzeselassie@gmail.com>
2023-12-16 11:24:45 -0800

    fix

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''alchemy python'''
+
+
+from sqlalchemy import Integer, MetaData, String, Column
+from sqlalchemy.ext.declarative import declarative_base
+from relationship_city import Base, City
+from sqlalchemy.orm import relationship
+
+
+
+class State(Base):
+    '''class to db'''
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, primary_key=True,
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
