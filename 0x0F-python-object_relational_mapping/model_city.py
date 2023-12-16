223629afa7735d0eb2edaccb3a1f38d988a6a9b1
bakiizese <bereketzeselassie@gmail.com>
2023-12-16 10:53:10 -0800

    fix

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''alchemy python'''
+
+
+from sqlalchemy import Integer, MetaData, String, Column, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class City(Base):
+    '''class to db'''
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, primary_key=True,
+                unique=True, nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
