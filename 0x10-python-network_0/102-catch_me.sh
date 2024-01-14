40e267a36ae39522504200152edcf053929f706e
bakiizese <bereketzeselassie@gmail.com>
2024-01-14 06:15:41 -0800

    fix

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# catvh
-curl -sL -X PUT -H "You got me!" -d "user_id=98" 0.0.0.0:5000/catch_me
+#chatch
+curl -s -X PUT -d "user_id=98" -H "Origin: You got me!" -L 0.0.0.0:5000/catch_me
