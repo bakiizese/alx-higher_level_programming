cbfc81c7d886e1ff1a8f8aac9ea79b36ee5e819b
bakiizese <bereketzeselassie@gmail.com>
2024-02-07 11:09:32 -0800

    js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,11 +1,11 @@
 $.ajax({
-    method:'GET',
-    url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
-    dataType: 'json'
-}).done(function(data){
-    $.map(data, function(elm, i){
-        if (i == 'name') {
-            $('DIV#character').text(elm);
-        }
-    });
+  method: 'GET',
+  url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+  dataType: 'json'
+}).done(function (data) {
+  $.map(data, function (elm, i) {
+    if (i == 'name') {
+      $('DIV#character').text(elm);
+    }
+  });
 });
--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,12 +1,12 @@
 $.ajax({
-    method:'GET',
-    url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
-    dataType: 'json'
-}).done(function(data){
-    $.map(data, function(elm, i){
-        for (let j in elm){
-            let title_name = elm[j].title
-            $('UL#list_movies').append('<li>' + title_name + '</li>');
-        }
-    });
+  method: 'GET',
+  url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+  dataType: 'json'
+}).done(function (data) {
+  $.map(data, function (elm, i) {
+    for (const j in elm) {
+      const title_name = elm[j].title;
+      $('UL#list_movies').append('<li>' + title_name + '</li>');
+    }
+  });
 });
